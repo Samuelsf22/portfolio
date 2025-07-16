d58f817b106c3bde98984feeb37b2a566407f736
SamuelSF22 <138950240+Samuelsf22@users.noreply.github.com>
2025-07-16 17:09:30 -0500

    update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,17 +3,18 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
-import tailwind from '@astrojs/tailwind';
-
 import icon from 'astro-icon';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
     icon(),
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
